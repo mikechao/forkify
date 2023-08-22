3b5bb83fb9234f8d2d689d1f6da48a8f4bd1d0f8
mikechao <mike.chao.one@gmail.com>
2023-08-22 12:22:45 -0700

    Initial copy from course

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0fe58a46-944a-41f2-b3c8-5b6458414195';
+export const MODEL_CLOSE_SEC = 2.5;
