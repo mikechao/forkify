826cee3e56d3be7132fc445713fda26e3aa97054
mike.chao.one@gmail.com <mike.chao.one@gmail.com>
2023-09-11 14:36:39 -0700

    Removed forkify api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '0fe58a46-944a-41f2-b3c8-5b6458414195';
 export const MODEL_CLOSE_SEC = 2.5;
 export const SPOON_API_KEY = 'cdb94e78674548eb8738b9ef2c526e0b';
 export const SPOON_API_URL =
